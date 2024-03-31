70f8014037598dec40d36b41167e6b45b2b163e1
mmmirellla <162386320+mmmirellla@users.noreply.github.com>
2024-03-31 19:48:16 -0300

    Program.cs
    add new cases

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Drawing;
 using Trabalho1;
 
@@ -8,7 +8,7 @@ do
 {
     try
     {
-        Console.WriteLine("\nEscolha uma opção:\n0 para sair \n1 para somar números\n2 para converter de metros para milímetros\n3 para fatorar um número de 1 a 10");
+        Console.WriteLine("\nEscolha uma opção:\n0) Sair\n1) Somar números\n2) Converter de metros para milímetros \n3) Calcular Aumento\n4) Calcular Desconto \n5) Aluguel Carro\n6) Calcular IMC\n7) Cantoneira até número fornecido\n8) Tabuada de cada número\n9) Múltiplos de 3 entre 0 e 100\n10) Fatoriais de 1 até 10\n11) Imposto de Renda\n12) Adivinhar número\n13) Financiamento do veículo\n14) Aposentadoria\n15) Jogo da velha ");
         int opcao = int.Parse(Console.ReadLine());
         int n; int m; float j;
 
@@ -23,7 +23,7 @@ do
                 {
                     Console.WriteLine("Precisa ser um número");
                 }
-                Console.WriteLine("Digite um número: ");
+                Console.WriteLine("Digite outro número: ");
                 while (!int.TryParse(Console.ReadLine(), out m))
                 {
                     Console.WriteLine("Precisa ser um número");
@@ -31,14 +31,14 @@ do
                 Console.WriteLine($"{n} + {m} = {Somatorio.Soma(n, m)}");
                 break;
             case 2:
-                Console.WriteLine("Quantos metros você tem?");
+                Console.WriteLine("Quantos metros você quer converter?");
                 while (!float.TryParse(Console.ReadLine(), out j))
                 {
                     Console.WriteLine("Precisa ser um número!");
                 }
                 Console.WriteLine($"{j} metros em milímetros é: {ConverteMparaMM.Conversor(j)}mm");
                 break;
-            case 3:
+            case 10:
                 Console.WriteLine("Qual número você quer fatorar?");
                 while (!int.TryParse(Console.ReadLine(), out n) || n < 1 || n > 10)
                 {
@@ -46,8 +46,19 @@ do
                 }
                 Console.WriteLine($"{n}! = {CalculoFatorial.Fatorial(n)}");
                 break;
+            case 14:
+                Console.WriteLine("Informe sua idade:");
+                while (!int.TryParse(Console.ReadLine(), out n))
+                {
+                    Console.WriteLine("Precisa ser um número!");
+                }
+                CalculoAposentadoria.Aposentadoria(n);
+                break;
+            case 15:
+                JogoDaVelha.Main();
+                break;
             default:
-                Console.WriteLine("Não entendi, tente outra vez"); break;
+                Console.WriteLine("Opção inválida, tente novamente."); break;
 
         }
 
