4a20f2939436ce923ad9ca6a8d0c6598f0371011
mmmirellla <162386320+mmmirellla@users.noreply.github.com>
2024-03-31 19:49:26 -0300

    Add files via upload

---
+++ CalculoAposentadoria.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho1
+{
+    public class CalculoAposentadoria
+    {
+        public static void Aposentadoria(int idade)
+        {
+            int idadeAposentadoria = 65;
+            if (idade >= idadeAposentadoria) 
+            {
+                Console.WriteLine("Você ja está apto para aposentar!");
+            }
+
+            else 
+            {
+                Console.WriteLine($"Você não tem idade suficiente para aposentar! \nFaltam { idadeAposentadoria - idade} anos para você se aposentar.");
+            }
+        }
+     }
+}
